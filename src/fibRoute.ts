68c720a560b90797c1b836d2554b096f54c1d108
Kensley Cadet <131932916+yeyadude7@users.noreply.github.com>
2024-08-27 10:43:55 -0400

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,10 +2,9 @@
 
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  //const { num } = req.params;
-
-  const num = req.params.num;
+export default (req: { params: { num: string; }; }, res: { send: (arg0: string) => void; }) => {
+  const { num } = req.params;
+  
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
